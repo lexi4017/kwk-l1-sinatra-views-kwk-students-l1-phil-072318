2969af39664718a088f185a464680c97777e213c
Alexsia Light <elightl729@comcast.net>
2018-07-31 18:42:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
 	end
+	get '/hello' do
+	  erb :hello
+  end
+  get '/goodbye' do
+    erb :goodbye
 end
